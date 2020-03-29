86360d2dc2c07e8c3d2a8211629ed15addaad13a
keiren <kzaracho@roshka.com>
2020-03-28 23:34:07 -0400

    add validation

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -16,7 +16,10 @@ struct AddView: View {
     @State private var name = ""
     @State private var type = "Personal"
     @State private var amount = ""
-
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     static let types = ["Business", "Personal"]
     
     var body: some View {
@@ -35,6 +38,7 @@ struct AddView: View {
             // Button that, when tapped, creates an ExpenseItem out of our properties and adds it to the expenses items.
             .navigationBarItems(trailing:
                 Button("Save") {
+                    self.validate()
                     if let actualAmount = Int(self.amount) {
                         let item = ExpenseItem(name: self.name, type: self.type,
                                 amount: actualAmount)
@@ -43,8 +47,45 @@ struct AddView: View {
                     }
                 }
             )
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            }
         }
     }
+    
+    func validate() {
+        
+        let varName = name
+        let varAmount = amount.trimmingCharacters(in: .whitespacesAndNewlines)
+       
+        guard !varName.isEmpty  else {
+            wordError(title: "Warning", message: "Name is empty")
+            return
+        }
+        
+        guard !varAmount.isEmpty  else {
+            wordError(title: "Warning", message: "Amount is empty")
+            return
+        }
+        
+        guard isValidNumber(text: varAmount) else {
+            wordError(title: "Warning", message: "Invalid amount")
+            return
+        }
+    }
+    
+    func isValidNumber(text: String) -> Bool{
+        let regex = "^[0-9]*$"
+        let test = NSPredicate(format:"SELF MATCHES %@", regex)
+        return test.evaluate(with: text)
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
 }
 
 struct  AddView_Previews: PreviewProvider {
